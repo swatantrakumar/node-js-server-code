8925a14c792181c6e3bff9b03874d70ccf016a0a
Swatantra Kumar <rajswatantra9@gmail.com>
2024-05-17 18:37:13 +0530

    update some file for authentication api

--- middleware/authenticateJWT.js
+++ middleware/authenticateJWT.js
@@ -1,11 +1,13 @@
 const jwt = require('jsonwebtoken');
-const { secretKey } = require('../utils/jwtUtils');
+const secretKey  = require('../utils/jwtUtils');
 
 function authenticateJWT(req, res, next) {
-    const token = req.headers.authorization;
+    // const token = req.headers.authorization;
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
 
     if (token) {
-        jwt.verify(token, secretKey, (err, decoded) => {
+        jwt.verify(token, secretKey.key, (err, decoded) => {
             if (err) {
                 return res.status(401).json({ message: 'Invalid token' });
             }
--- utils/commonUtils.js
+++ utils/commonUtils.js
@@ -1,11 +1,24 @@
+const path = require('path');
+const { pathToFileURL } = require('url');
+
 // Function to dynamically import models based on path
 async function getModel(filePath) {
-    const absolutePath = path.resolve(__dirname, filePath);
-    const model = await import(absolutePath);
+    const projectRoot = process.cwd();
+    const absolutePath = path.resolve(projectRoot, filePath);
+
+    // Validate the file path and extension
+    if (!absolutePath.endsWith('.js') && !absolutePath.endsWith('.mjs')) {
+        throw new Error('The file path must end with .js or .mjs');
+    }
+
+    // Convert the absolute path to a file URL
+    const fileUrl = pathToFileURL(absolutePath).href;
+
+    const model = await import(fileUrl);
     return model.default;
 }
 function decodeBase64(encodeData){
     return JSON.parse(Buffer.from(encodeData, 'base64').toString('utf-8'));
 }
 
-module.exports = {decodeBase64};
+module.exports = {decodeBase64,getModel};

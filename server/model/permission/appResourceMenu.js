b08b29bcbf1607cf12c9c64f5ca95734204f9353
Swatantra Kumar <rajswatantra9@gmail.com>
2024-08-06 18:07:05 +0530

    update code for get permissin

--- server/model/permission/appResourceMenu.js
+++ server/model/permission/appResourceMenu.js
@@ -15,6 +15,39 @@ const AppResourceMenu = mongoose.Schema({
     }
 });
 
+AppResourceMenu.virtual('details')
+    .get(function () {
+        return this._details;
+    })
+    .set(function (value) {
+        this._details = value;
+    });
+
+    AppResourceMenu.virtual('submenuMap')
+    .get(function () {
+        return this._menuMap;
+    })
+    .set(function (value) {
+        this._menuMap = value;
+    });
+    AppResourceMenu.virtual('templateTabMap')
+    .get(function () {
+        return this._menuMap;
+    })
+    .set(function (value) {
+        this._menuMap = value;
+    });
+
+
+    AppResourceMenu.set('toJSON', {
+        virtuals: true,
+        versionKey: false,  // remove __v
+        transform: function (doc, ret) {   
+            delete ret.id;   // remove `id` field
+            return ret;
+        }
+    });
+
 
 // Extend the User model with methods from the base entity
 // User.prototype = Object.assign(User.prototype, BaseEntity);

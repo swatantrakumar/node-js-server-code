1153e63a7f46a2f839e3676a3f3db7fcf889c8ef
Swatantra Kumar <rajswatantra9@gmail.com>
2024-08-11 13:22:25 +0530

    update code for geting template

--- server/routes/dataRoutes.js
+++ server/routes/dataRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getData, getGridData, genericSearch } = require('../controllers/dataController');
+const {genericSearch ,getDataForGrid, getMultiGridData } = require('../controllers/dataController');
 
 // router.get('/data', getData);
 // router.post('/data', postData);
 
-router.post('/gd', getGridData);
+router.post('/gd/:orderBy', getDataForGrid);
 router.post('/sobj', genericSearch);
+router.post('/sobj/:orderBy', genericSearch);
+router.post('/gd_list', getMultiGridData);
 
 module.exports = router;

6ee84f0d8852edeb77bf0c357c15609b10cda7a7
Swatantra Kumar <rajswatantra9@gmail.com>
2024-08-12 18:43:30 +0530

    update code for gsd call

--- server/handler/queryHandler/retrievalQueryHandler.js
+++ server/handler/queryHandler/retrievalQueryHandler.js
@@ -70,7 +70,7 @@ class RetrievalQueryHandler{
                 //         for (int i = 0; i < array.length(); i++) {
                 //             objList.add(templateHandler.templateIdMap.get(array.getJSONObject(i).getString("_id")));
                 //         }
-                //     } catch (Exception e) {
+                //     } catch (e) {
                 //         e.printStackTrace();
                 //     }
                 //     result = objList;
@@ -148,7 +148,7 @@ class RetrievalQueryHandler{
                 //             JSONObject templateJson = array.getJSONObject(i);
                 //             objList.add(templateHandler.getFormMap().get(templateJson.getString("_id")));
                 //         }
-                //     } catch (Exception e) {
+                //     } catch (e) {
                 //         e.printStackTrace();
                 //     }
                 //     result = objList;
@@ -242,12 +242,13 @@ class RetrievalQueryHandler{
     }
     async getStaticDataResultForCoreMasters(result, employee, kvp, resultList, sub_result, values, value){
         if(value.toUpperCase().includes("QTMP")){
-			invokeQtmp(result, employee, kvp, resultList, sub_result, values, value);
+			// invokeQtmp(result, employee, kvp, resultList, sub_result, values, value);
+            console.log("qtmp call section!!!")
 		}else {
 			this.prepareCoreKvpForStaticDataSearch(kvp, sub_result, values, value);
 			const sortBy = this.getDefaultSortByString(value);
-			const data_list = await this.processApplicationSobjCall(employee,sortBy, kvp);
-			this.prepareCoreReponseDataForStaticDataCallMasters(false,result, employee, kvp, resultList, sub_result, values, value, data_list);
+			let data_list = await this.processApplicationSobjCall(employee,sortBy, kvp);
+			await this.prepareCoreReponseDataForStaticDataCallMasters(false,result, kvp, resultList, sub_result, value, data_list);
 		}
     }
     prepareCoreKvpForStaticDataSearch(kvp, sub_result, values, value) {
@@ -317,8 +318,259 @@ class RetrievalQueryHandler{
             } );
         }
     }
-    prepareCoreReponseDataForStaticDataCallMasters(){
-        
+    async prepareCoreReponseDataForStaticDataCallMasters(include_all, result, kvp, resultList, sub_result, value,data_list){
+        const isCompleteObject = "COMPLETE_OBJECT" === this.checkType(kvp);
+        if (data_list != null) {
+            switch (value){
+                case "country":
+                case "state":
+                case "city":
+                case "adm":
+                    if(include_all){
+                        resultList.push("All");
+                    }
+                    data_list.forEach(object => {
+                        // Convert object to a plain JavaScript object (equivalent to LinkedHashMap in Java)
+                        const template = JSON.parse(JSON.stringify(object));
+                    
+                        // Create a JSONObject equivalent (template is already a plain object)
+                        const il = template;
+                    
+                        // Check if "keyValue" is null or undefined
+                        if (!il.hasOwnProperty("keyValue") || il["keyValue"] === null) {
+                            resultList.push(il["name"].toString());
+                        } else {
+                            sub_result["data"] = template["keyValue"];
+                        }
+                    });
+                    break;
+                case "available_status":
+                    kvp.value = "status_master";
+                    data_list.forEach(object => {
+                        const statusMaster = JSON.parse(JSON.stringify(object));
+                        resultList.push(statusMaster.current_status);
+                        resultList.push(...statusMaster.available_status);
+                    });
+                    break;
+                case "form_field":
+                case "grid_field":
+                case "grid":
+                case "form":
+                case "tab":
+                case "menu_template":
+                case "form_template":
+                    resultList.push(...data_list);
+                    break;
+                case "project_module":
+                    data_list.forEach(object => {
+                        const template = JSON.parse(JSON.stringify(object));
+                        const il = template;
+                        try {
+                            try {
+                                il.set("name", il.title);
+                            }catch (e){
+                                console.log("Error while putting prject module name {}", e.message);
+                            }
+                            const obj = this.getReferenceJsonObjectAsNamePicker(value,il);
+                            resultList.push(obj);
+                        }catch (e){
+                            console.log("Error while adding reference in the list : {} {}", value,il);
+                        }
+                    });
+                    break;
+                case "account_city":
+                    data_list.forEach(object => {
+                        const template = JSON.parse(JSON.stringify(object));
+                        const il = template;
+                        try {
+                            if(il.city) resultList.push(il.city);
+                        }catch (e){
+                            console.log("Error while adding reference in the list : {} {}", value,il);
+                        }
+                    })
+                    break;
+                case "lead_contact":
+                    data_list.forEach(object => {
+                        const template = JSON.parse(JSON.stringify(object));
+                        const il = template;
+                        try {
+                            const obj = this.getReferenceJsonObjectAsNamePicker(value,il);
+                            const name  = il.first_name;
+                            if(il.last_name)
+                                name =  name  + " " + il.last_name;
+                            obj.set("name", name);
+                            if(isCompleteObject){
+                                obj.set('COMPLETE_OBJECT',object);
+                            }
+                            resultList.push(obj);
+                        }catch (e){
+                            console.log("Error while adding reference in the list : {} {}", value,il);
+                        }
+                    })
+                    break;
+                case "account_list" :
+                    data_list.forEach(object => {
+                        const template = JSON.parse(JSON.stringify(object));
+                        const il = template;
+                        try {
+                            const obj = commonUtils.getReferenceJsonObject(il);
+                            const name  = obj.name.toString();
+                            if(il.city)
+                                name =  name  + "/" + il.city;
+                            obj.set("name", name);
+                            if(isCompleteObject){
+                                obj.set('COMPLETE_OBJECT',object);
+                            }
+                            resultList.push(obj);
+                        }catch (e){
+                            console.log("Error while adding reference in the list : {} {}", value,il);
+                        }
+                    })
+                    break;
+                default:
+                    if(include_all){
+                        resultList.push(commonUtils.allobject);
+                    }
+                    if(kvp.data_template) {
+                        this.fetchDataAsDataTemplate(value,kvp, resultList, data_list);
+                    }else if(cacheService.retriveAsStringList(value)){
+                        this.getListAsName(resultList, value, data_list);
+                    }else {
+                        this.retrieveDataASReferenceObject(resultList, value, data_list);
+                    }
+                    break; 
+            }
+            if(sub_result.get("data")==null) {
+                sub_result.set( "data", resultList);
+            }
+            result.get("success").push(commonUtils.mapToObj(sub_result));
+        }else{
+            switch(value){
+                case "status" :
+                    resultList.push( "Active");
+                    resultList.push("Inactive");
+                    sub_result.set( "data", resultList);;
+                    result["success"] =  JSON.parse(JSON.stringify(sub_result));
+                    break;
+                case "class_fields":
+                    if(kvp.crList && Array.isArray(kvp.crList) && kvp.crList.length > 0){
+                        const searchCriteria = kvp.crList[0]; 
+                        if(searchCriteria.fName == "name" && searchCriteria.fValue){
+                            const colName = searchCriteria.fValue;
+                            try {
+                                const clazz = await commonUtils.getModel(colName);
+                                resultList.push(...commonUtils.getFieldList(clazz));                                
+                            } catch (e) {
+                                console.error(e.stack);
+                            }
+                            sub_result.set( "data", resultList);;
+                            result["success"] =  JSON.parse(JSON.stringify(sub_result));
+                        }
+                        else {
+                            sub_result.set( "data", [] );
+                            result["success"] =  JSON.parse(JSON.stringify(sub_result));
+                        }
+                    }
+                    else {
+                        sub_result.set("data", []);
+                        result["success"] =  JSON.parse(JSON.stringify(sub_result));
+                    }                    
+                    break;
+                default:
+                    sub_result.set("data", []);
+                    result["success"] =  JSON.parse(JSON.stringify(sub_result));
+                    break;
+            }
+        }
+    }
+    fetchDataAsDataTemplate(value, kvp, resultList, data_list) {
+        switch (this.checkType(kvp)){
+            case "COMPLETE_OBJECT":
+                data_list.forEach(object => {
+                    try {
+                        const il = JSON.parse(JSON.stringify(object));
+                        const obj = this.getReferenceJsonObjectAsNamePicker(value, il);
+                        obj.set("COMPLETE_OBJECT",object);
+                        resultList.push(obj);
+                    } catch (e) {
+                        console.log("Error while adding reference in the list : {} {}", value);
+                    }
+                });
+                break;
+            case "OBJECT_FOR_LIST":
+                try{
+                    resultList.push(...data_list);
+                }catch (e){
+                    console.log("Error while adding reference in the list : {} {}", value);
+                }
+                break;
+            default:
+                const dataConfig = cacheService.export_configuration.get(kvp.data_template.toUpperCase());
+                const colMap = new Map();
+                data_list.forEach(object => {                
+                    try {
+                        const il = JSON.parse(JSON.stringify(object));
+                        const obj = new Map();
+                        freeMarkerReportService.getHashMapFromJsonObjectAsPerTemplate(dataConfig.getColumnLists(), il, obj,colMap);
+                        if(dataConfig.isValuesOnly()) {
+                            if(obj) {
+                                if (obj?.values?.length == 1)
+                                    resultList.push(Object.values(obj).join(","));
+                                else
+                                    resultList.push(...obj.values);
+                            } else resultList.push("");
+                        }
+                        else
+                            resultList.push(obj);
+                    } catch (e) {
+                        console.log("Error while adding new Object in resultList {}", e.message);
+                    }
+                });
+        }
+
+    }
+    getReferenceJsonObjectAsNamePicker(collection, il) {
+        const pojoMaster = cacheService.getPojoFromCollection(collection);
+        const name = "";
+        const fields = pojoMaster.fields_for_reference;
+        if(fields && Array.isArray(fields) && fields.length > 0) {
+            for (let i = 0; i < fields.length; i++) {
+                try {
+                    const fieldValue = commonUtils.getValueFromJSONObject(il,fields[i]);
+                    if(fieldValue) {
+                        name = name + pojoMaster.field_splitter + fieldValue;
+                    }
+                } catch (e) {
+                    console.log("Error while fetching value from object {}");
+                }
+            }
+            name = name.substring(pojoMaster.field_splitter.length);
+        }
+        return commonUtils.getReferenceJsonObject(il,name);
+    }
+    getListAsName(resultList, value, data_list) {
+        try {
+            data_list.forEach(object => {
+                const il = JSON.parse(JSON.stringify(object));
+                resultList.push(this.getReferenceJsonObjectAsNamePicker(value, il).get("name"));
+            })
+        }catch (e){
+            console.error(e.stack);
+        }
+    }
+    retrieveDataASReferenceObject(resultList, value, data_list){
+        data_list.forEach(object => {
+            const il = JSON.parse(JSON.stringify(object));
+            try {
+                const obj = this.getReferenceJsonObjectAsNamePicker(value, il);
+                resultList.push(obj);
+            } catch (e) {
+                console.log("Error while adding reference in the list : {} {}", value, il);
+            }
+        });
+    }
+    checkType(kvp){
+        return kvp.data_template != null ? kvp.data_template.toUpperCase():"";
     }
 }
 module.exports = RetrievalQueryHandler;
---
+++ server/model/generic/columnList.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ColumnListSchema = new Schema({
+    label: { type: String },
+    columnType: { type: String },
+    columnValue: { type: String },
+    defaultValue: { type: String },
+    createDefaultReference: { type: Boolean, default: true },
+    position: { type: Number },
+    columnName: { type: String },
+    convertValueCaseTo: { type: String },
+    separator: { type: String },
+    columnHeading: { type: String },
+    collectionName: { type: String },
+    format: { type: String },
+    source_format: { type: String },
+    fieldsFromObject: [{ type: String }],
+    index: { type: Number },
+    additionalDetails: {
+        type: Map,
+        of: String
+    },
+    columnList: [this],
+    updateable: { type: Boolean },
+    searchCriteriaList: [{
+        // Define the SearchCriteria schema if needed
+    }],
+    conditions: [{
+        // Define the ConditionalCriteria schema if needed
+    }],
+    value: Schema.Types.Mixed, // Use Mixed for Object type
+    image_height: { type: Number },
+    image_width: { type: Number },
+    columnColor: { type: Number, min: -32768, max: 32767 } // short equivalent in Java is 16-bit integer
+});
+
+ColumnListSchema.methods.toString = function() {
+    return `ColumnList{columnType='${this.columnType}', columnValue='${this.columnValue}', defaultValue='${this.defaultValue}', createDefaultReference=${this.createDefaultReference}, position=${this.position}, columnName='${this.columnName}', convertValueCaseTo='${this.convertValueCaseTo}', separator='${this.separator}', columnHeading='${this.columnHeading}', collection='${this.collection}', fieldsFromObject=${this.fieldsFromObject}, index=${this.index}, additionalDetails=${this.additionalDetails}, columnList=${this.columnList}}`;
+};
+
+module.exports =  ColumnListSchema;

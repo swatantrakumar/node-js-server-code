b716cd8c57406adff37622647b10f994ffdd11cf
Swatantra Kumar <rajswatantra9@gmail.com>
2024-08-21 12:00:46 +0530

    update config file

---
+++ server/enum/config.js
@@ -0,0 +1,33 @@
+// Enum for various constant values in your project
+const Config = {
+    // MongoDB connection settings
+    MONGODB_URI: 'mongodb://127.0.0.1:27017',
+    DATABASE_NAME: 'central-elabs-prod',
+    COLLECTION_NAME: 'yourCollectionName',
+    
+    // Other configuration values
+    PORT: 3000,
+    
+    // User credentials (note: sensitive data like passwords should be handled carefully)
+    DB_USER: 'yourDatabaseUsername',
+    DB_PASSWORD: 'yourDatabasePassword',
+    
+    // Other enums and constant values
+    STATUS_ACTIVE: 'active',
+    STATUS_INACTIVE: 'inactive',
+    ROLE_ADMIN: 'admin',
+    ROLE_USER: 'user',
+    PACKAGE_PATH : 'server/model/',
+    DEFAULT_REFCODE : 'MCLR01',
+    STORAGE_ROOT_PATH : 'e-labs-prod-documents',
+    AWS : {
+      BUCKET_PREFIX : 'prod/appId/refCode/COLLECTION/series/serialId'
+    },
+    FILE_SYSTEM: 'S3',
+    AWS_CONFIG : {      
+      region: 'ap-south-1'
+    }
+  };
+  
+  // Export the config object
+  module.exports = Config;
